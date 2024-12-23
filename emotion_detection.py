5c4467a7318bfc9cd30412a2348e04c8d40536f1
ab-laprairie <ab.laprairie@gmail.com>
2024-12-23 11:37:24 -0500

    Initial Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,6 +7,25 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url,json = myobj, headers=header)
-    return response.text
-
     
+    formatted_response = json.loads(response.text)
+
+    # Extract the emotion scores
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+
+    # Find the dominant emotion
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    # Format the output
+    output = {
+        'anger': emotion_scores['anger'],
+        'disgust': emotion_scores['disgust'],
+        'fear': emotion_scores['fear'],
+        'joy': emotion_scores['joy'],
+        'sadness': emotion_scores['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+    # Print the result
+    return(output)
+
+
